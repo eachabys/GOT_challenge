5f4b2d1a56e2ea2d0f0fd3190d6a42a04fc62ad7
eachabys <44489101+eachabys@users.noreply.github.com>
2019-07-18 23:40:46 -0400

    Add files via upload

--- src/01_got_spark_dictionary.py
+++ src/01_got_spark_dictionary.py
@@ -14,57 +14,54 @@ import operator
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql.window import Window
-from itertools import chain
-from collections import Counter
 
 #set up Input/Output parameters
 #Location of all input data
 source_bucket='got-challenge-insight'
+file_lists=['12','13']
 
-def get_dictionary(sc):
+
+def get_data(sc):
     """Read the data from the source as words with unique indexes"""
     print("STARTTTTTTTTTTTTT")
-    
-    #read all the data from input directory
-    rdd=sc.textFile('s3a://'+source_bucket+'/*')
+    indexed_documents=[]
+    for i in file_lists:
+        #read all the data from input directory
+        rdd=sc.textFile('s3a://'+source_bucket+'/'+i+'*')
+        #get rdd for each single word in an input file. 
+        #assign each word a special index
+        rdd=rdd.map(lambda x: x.encode("utf-8", "ignore"))\
+          .flatMap(lambda x: (''.join(x)).split(' '))\
+          .map(lambda word: re.sub(ur"[^\w\d'\s]+", '', word))\
+          .map(lambda word: re.sub(r'\d+', '', word))\
+          .filter(lambda word: len(word)>0)\
+          .map(lambda word: (word.lower(),i))\
+          .distinct()\
+          .zipWithIndex()
+        
+        #assign unique indexes to each word
+        rddindex=rdd.map(lambda word: str(word).split(',')[2])\
+          .map(lambda index: index.replace(')',''))\
+          .map(lambda index: index)
 
-    #get rdd for each single word in an input file. 
-    #assign each word a special index
-    rdd=rdd.map(lambda x: x.encode("utf-8", "ignore"))\
-      .flatMap(lambda x: (''.join(x)).split(' '))\
-      .map(lambda word: re.sub(ur"[^\w\d'\s]+", '', word))\
-      .map(lambda word: re.sub(r'\d+', '', word))\
-      .filter(lambda word: len(word)>0)\
-      .map(lambda word: word.lower())\
-      .distinct()\
-      .zipWithIndex()
-    print rdd.take(20)
-    return(rdd)
-
-def invert_indexes(sc):
-    """For each input file generate a set of words with corresponding indexes"""
-    print("STARTTTTTTTTTTTTT")
+        #collect list of files (documents) with words and corresponding unique ids
+        indexed_documents.append((i,rddindex.collect()))
     
-    #group set of words with unique ids by corresponding book ID (unique filename) 
-    rdd=rdd.map(lambda x: x.encode("utf-8", "ignore"))\
-      .flatMap(lambda x: (''.join(x)).split(' '))\
-      .map(lambda word: re.sub(ur"[^\w\d'\s]+", '', word))\
-      .map(lambda word: re.sub(r'\d+', '', word))\
-      .filter(lambda word: len(word)>0)\
-      .map(lambda word: word.lower())\
-      .distinct()\
-      .zipWithIndex()
-    #print rdd.take(20)
-    return(rdd)
+    return(rdd,indexed_documents)
 
 def output_data():
     #Save the outputs
-    print("CONTINUEEEEEEEEEEEE")
-    rdd=get_dictionary(sc) 
-    words_dictionary=rdd.collect()
+    print("OUT OUT OUT")
+    dictionary=get_data(sc)[0]
+    indexed_documents=get_data(sc)[1]
+    #print rdd.take(20)
+    words_dictionary=dictionary.collect()
     file1=open(output1,"w+")
     for row in words_dictionary:
-    	file1.write(' '.join(str(r) for r in row) + "\n")
+        file1.write(' '.join(str(r) for r in row) + "\n")
+    file2=open(output2,"w+")
+    for row in indexed_documents:
+        file2.write(' : '.join(str(r) for r in row) + "\n")
 
 if __name__ == '__main__':
     start_time=time.time()
@@ -72,5 +69,6 @@ if __name__ == '__main__':
     spark_session = SparkSession.builder.appName("indexing").getOrCreate()
     sqlContext = SQLContext(sc)
     output1='dictionary.txt'
+    output2='inverted_index.txt'
     output_data()
     
